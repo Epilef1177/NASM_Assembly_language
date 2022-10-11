c6ac486dca83f4f239ab66eff8bb262a2e45516c
Epilef1177 <fangeloferreira2023@gmail.com>
2022-10-11 12:04:55 -0300

    Compilando e linkeditando pelo terminal pela primeira vez

--- Curso Nasm/Aula 1/Hello World.asm
+++ Curso Nasm/Aula 1/Hello World.asm
@@ -1,4 +1,6 @@
-%include "io.inc"
+;%include "io.inc"
+;COMPILAR: nasm -f elf64 nome_arquivo.asm
+;LINKEDITAR: ld -s -o nome_executavel nome_arquivo.o
 section .data   ;Define todas as constantes
     msg0 db '', 0xA,0xA
     msg1 db 'Hello World!', 0xA
@@ -6,12 +8,12 @@ section .data   ;Define todas as constantes
     msg2 db 'hello world', 0xA
     tam2 equ $- msg2
     tam0 equ $- msg0
-section .bss    ;Define todas as variáveis
+;section .bss    ;Define todas as variáveis
 
 section .text   ;Começa o programa
-global CMAIN    ;Label Global
-CMAIN:
-    mov ebp, esp; for correct debugging
+global _start    ;Label Global
+_start:
+    ;mov ebp, esp; for correct debugging
     ;write your code here
     mov eax, 0x4
     mov ebx, 0x1
@@ -33,5 +35,8 @@ CMAIN:
     
     
 _saida:    
-    xor eax, eax
-    ret
+    ;xor eax, eax
+    ;ret
+    mov eax, 0x1
+    mov ebx, 0x0
+    int 0x80
