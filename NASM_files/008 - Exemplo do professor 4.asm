19d9ee67ac5e92be0bb787c43e66acd85fb95b9b
Epilef1177 <fangeloferreira2023@gmail.com>
2022-11-30 18:21:56 -0300

    comentários até 009

--- NASM_files/008 - Exemplo do professor 4.asm
+++ NASM_files/008 - Exemplo do professor 4.asm
@@ -10,57 +10,56 @@ section .data
 section .text
 global CMAIN
 CMAIN:
-    mov ebp, esp; for correct debugging
+    mov ebp, esp                ; for correct debugging
     
-    cld
+    cld                         ;DF = 0
     PRINT_STRING msg1
     NEWLINE
-    mov esi, msg1
+    mov esi, msg1               ;esi = "H"  |   esi aponta para msg1
     PRINT_STRING msg2
     NEWLINE
-    mov edi, msg2
-    mov ecx, tam1
-    repe cmpsb
-    je true
-    jne false
+    mov edi, msg2               ;edi = "H"  |   edi aponta para msg2
+    mov ecx, tam1               ;ecx = tam1 |   tamanho das strings a serem comparadas
+    repe cmpsb                  ;repete a comparação com todos os caracteres até o ecx igual à 0. ecx-- a cada interação
+    je true                     ;se as strings forem iguais vai para a label "true"
+    jne false                   ;se as strings não forem iguais vai para a label "false"
 true:
-    call equal
-    jmp next
+    call equal                  ;chama a função equal
+    jmp next                    ;pula para o label "next"
 false:    
-    call not_equal
+    call not_equal              ;chama a função not_equal
 
 next:
-    cld
+    cld                         ;DF = 0
     PRINT_STRING msg1
     NEWLINE
-    mov esi, msg1
+    mov esi, msg1               ;esi = "H"  |   esi aponta para msg1  
     PRINT_STRING msg3
     NEWLINE
-    mov edi, msg3
-    mov ecx, tam1
-    repe cmpsb
-    je true1
-    jne false2
+    mov edi, msg3               ;edi = "H"  |   edi aponta para msg3
+    mov ecx, tam1               ;ecx = tam1 |   tamanho das strings a serem comparadas
+    repe cmpsb                  ;repete a comparação com todos os caracteres até o ecx igual à 0. ecx-- a cada interação
+    je true1                    ;se as strings forem iguais vai para a label "true"
+    jne false2                  ;se as strings não forem iguais vai para a label "false"
 true1:
-    call equal
-    jmp next
+    call equal                  ;chama a função equal
+    jmp next                    ;pula para o label "next"
 false2:    
-    call not_equal
-    jmp ending_program
+    call not_equal              ;chama a função not_equal
+    jmp ending_program          ;pula para o fim do programa
     
 ;saídas pré-definidas    
 equal:
-    PRINT_STRING igual
+    PRINT_STRING igual          ;confirma que as strings são iguais
     NEWLINE
     NEWLINE
     ret
 not_equal:
-    PRINT_STRING nao_igual
+    PRINT_STRING nao_igual      ;confirma que as strings são diferentes
     NEWLINE
     NEWLINE
     ret
     
-;fim do programa
 ending_program:
     xor eax, eax
     ret
--- NASM_files/009 - Comparando duas Strings e manipulando-as.asm
+++ NASM_files/009 - Comparando duas Strings e manipulando-as.asm
@@ -3,9 +3,9 @@
 section .data
     ;sapce = string que compararemos para verificar os espaços
     space db "                                                                                                                                 ", 0
-    star db "*", 0          ;star = * deve
-    msg db "Do you speak english? No?!", 0
-    tam equ $- msg
+    star db "*", 0          ;star = *   |   o asterístico deve substituir os espaços
+    msg db "Do you speak english? No?!", 0  ;frase a ter os espaços substituídos
+    tam equ $- msg  ;tamanho da string msg
 section .text
 global CMAIN
 CMAIN:
@@ -13,24 +13,25 @@ CMAIN:
     mov ecx, tam            ;ecx = length de msg
     mov eax, 0              ;eax = 0    |   eax guardará o valor da posição na string que está sendo comparada
 _start:
-    mov esi, space          ;
-    mov edi, msg
-    add esi, eax
-    add edi, eax
+    mov esi, space          ;esi = " "  |   esi é o ponteiro para space
+    mov edi, msg            ;edi = "D"  |   edi é o ponteiro para msg
+    add esi, eax            ;esi += eax |   o eax redireciona o ponteiro caso ele tenha perdido a localização do endereço
+    add edi, eax            ;edi += eax |   o eax redireciona o ponteiro caso ele tenha perdido a localização do endereço
 _fixing_space:
-    inc eax
-    cmpsb
-    je fixing
-    loop _fixing_space
-    jmp _ending_program
-    ;jne not_equal
+    inc eax                 ;eax++      |   o eax é incrementado de um em um byte para salvar o ponteiro para a localização atual na string
+    cmpsb                   ;esi == edi |   compara os caracteres em nos dois registradores especiais
+    je fixing               ;se esi == edi então pula pra função fixing
+    loop _fixing_space      ;caso contrário, volta pro loop sem fazer nada no label "_fixing_space"
+    jmp _ending_program     ;após retirar todos os espaços e substituir por asterístico vai para o fim do programa
+
+;substitui o espaço por asterístico
 fixing:
-    mov esi, star
-    dec edi
-    movsb
+    mov esi, star           ;esi = "*"  |   símbolo para substituir o espaço
+    dec edi                 ;edi--      |   como no último comando 'cmpsb' somou +1 ao ponteiro edi, o edi não aponta mais para o vazio, tendo que voltar um byte
+    movsb                   ;edi = esi  |   espaço recebe asterístico
     PRINT_STRING msg
     NEWLINE 
-    jmp _start
+    jmp _start              ;retorna para a função principal à procura de novos espaços
     
 _ending_program:
     xor eax, eax
